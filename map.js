b53aa1d80bb2307ef60a90cb1f8913b44aa66032
Kevin Allioli <kevin@linit.io>
2020-03-21 10:47:16 +0100

    Add index.html and map.js

---
+++ map.js
@@ -0,0 +1,24 @@
+
+     let url = "https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=bc73101ff7dfe7125859e4a2dc3f0c0bc3921294";
+     function initMap() {
+
+	  var map = new google.maps.Map(document.getElementById('map'), {
+	    zoom: 14,
+	    center: {lat: 45.75, lng: 4.85}
+	  });
+	 
+	  let ajax = new XMLHttpRequest();
+        ajax.open("GET",url,true);
+        ajax.addEventListener("load", ()=>{
+        	let data = JSON.parse(ajax.responseText);
+        	console.log(data);
+        	for (let i=0; i<data.length; i++){
+        		var marker = new google.maps.Marker({
+			    position: {lat: data[i].position.lat, lng: data[i].position.lng},
+			    map: map
+	  });
+        	}
+        
+        });
+        ajax.send();
+	}
