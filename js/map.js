89f1a4c73765a85fa8ce9170efe1b0fc88df5f61
Kevin Allioli <kevin@linit.io>
2020-05-09 02:26:18 +0200

    Add windows menu for map on click

--- js/map.js
+++ js/map.js
@@ -21,7 +21,8 @@ function initMap() {
 				stationStatus: data[i].status,
 				stationTotalStand: data[i].bike_stands,
 				stationStandAvailable: data[i].available_bike_stands,
-				stationBikeAvailable: data[i].available_bikes
+				stationBikeAvailable: data[i].available_bikes,
+				stationBanking: data[i].banking
 			});
 
 			marker.addListener("click", function() {
@@ -33,15 +34,32 @@ function initMap() {
 				if (marker.stationStatus == 'OPEN') {
 					document.getElementById("station-status").innerHTML = 'ouverte';
 					document.getElementById("station-status").className = 'station-open';
-				} else if (smarker.stationStatus == 'CLOSE') {
+				} else if (marker.stationStatus == 'CLOSE') {
 					document.getElementById("station-status").innerHTML = 'fermée';
 					document.getElementById("station-status").className = 'station-close';
 				} 
 
+				if (marker.stationBanking == 'true') {
+					document.getElementById("station-banking").style.color = "green";
+				} else if (marker.stationBanking == 'false') {
+					document.getElementById("station-banking").style.color = "red";
+				}
+
 				document.getElementById("station-address").innerHTML = marker.stationAddress;
 				document.getElementById("station-available-bike").innerHTML = marker.stationBikeAvailable;
 				document.getElementById("station-total-stand").innerHTML = marker.stationTotalStand;
+				document.getElementById('info-container').style.width = "300px";
+				document.getElementById('station-details').style.display = "block";
 				});
+
+			let closeStatus;
+			if (document.getElementById('info-container').style.width == "0px") {
+				let closeStatus = 0;
+			} else if (document.getElementById('info-container').style.width == "300px") {
+				let closeStatus = 1;
+			}
+			console.log(closeStatus);
+			
 		}
 	});
 
