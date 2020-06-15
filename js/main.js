c210373490edde26aada78a9c6b0e182dc8ef73b
Kevin Allioli <kevin@linit.io>
2020-06-15 16:05:37 +0200

    Session du 15/06/2020

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-let map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
-        center: {lat: 47.2173, lng: -1.5534}
-    });
-    let mapFull = new bikeMap(map);
+function initBikeMap() {
+    var mapFull = new bikeMap('map',-1);
   }
--- js/map.js
+++ js/map.js
@@ -1,75 +1,87 @@
 class bikeMap {
 	
-	constructor (map){
+	constructor (nameDiv,nbMarker){
 		this.url = "https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=bc73101ff7dfe7125859e4a2dc3f0c0bc3921294";
 		this.i;
-		this.map=map;
-		this.initMap();
-		this.stationNameFull;
-		this.stationNameCut;
+		this.map;
+		this.initMap(nameDiv,nbMarker);
 	}
+	
+	
+	addinfoMarker(vlat,vlng,map,stationId,stationName,stationAddress,stationStatus,stationTotalStand,stationStandAvailable,stationBikeAvailable,stationBanking){
+		let marker = new google.maps.Marker({
+			position: {lat: vlat, lng: vlng},
+			map: map,
+			stationId: stationId,
+			stationName: stationName,
+			stationAddress: stationAddress,
+			stationStatus: stationStatus,
+			stationTotalStand: stationTotalStand,
+			stationStandAvailable: stationStandAvailable,
+			stationBikeAvailable: stationBikeAvailable,
+			stationBanking: stationBanking
+		});
+			
+		google.maps.event.addListener(marker,"click", (e)=> {
 
-	initMap() {
+			marker.stationName = marker.stationName.split('-');
+			document.getElementById("station-name").innerHTML = marker.stationName[1];
+	
+			if (marker.stationStatus == 'OPEN') {
+				document.getElementById("station-status").innerHTML = 'ouverte';
+				document.getElementById("station-status").className = 'station-open';
+			} else if (marker.stationStatus == 'CLOSE') {
+				document.getElementById("station-status").innerHTML = 'fermée';
+				document.getElementById("station-status").className = 'station-close';
+			}
+	
+			this.banking = marker.stationBanking;
+			if (this.banking) {
+				document.getElementById("station-banking").style.color = "black";
+			} else if (!this.banking) {
+				document.getElementById("station-banking").style.color = "grey";
+			}
+	
+			document.getElementById("station-address").innerHTML = marker.stationAddress;
+			document.getElementById("station-available-bike").innerHTML = marker.stationBikeAvailable;
+			document.getElementById("station-total-stand").innerHTML = marker.stationTotalStand;
+			document.getElementById('info-container').style.width = "300px";
+			document.getElementById('station-details').style.display = "block";
+		});
+	
+		document.getElementById("close").addEventListener('click', (e)=> {
+		document.getElementById('info-container').style.width = "0px";
+		document.getElementById('station-details').style.display = "none";
+		});
+	
+		document.getElementById("station-btn").addEventListener('click', (e)=> {
+		document.getElementById("canvas-section").style.display = "block";
+	});
 		
+}
+
+	initMap(nameDiv,nbMarker) {
+		this.map = new google.maps.Map(document.getElementById(nameDiv), {
+			zoom: 14,
+			center: {lat: 47.2173, lng: -1.5534}
+		});
+		 
 		this.ajax = new XMLHttpRequest();
 		this.ajax.open("GET",this.url,true);
 		this.ajax.addEventListener("load", ()=>{
 			this.data = JSON.parse(this.ajax.responseText);
-			console.log(this.data);
-			for (this.i=0; this.i<this.data.length; this.i++){
-				this.marker = new google.maps.Marker({
-					position: {lat: this.data[this.i].position.lat, lng: this.data[this.i].position.lng},
-					map: this.map,
-					stationId: this.data[this.i].number,
-					stationName: this.data[this.i].name,
-					stationAddress: this.data[this.i].address,
-					stationStatus: this.data[this.i].status,
-					stationTotalStand: this.data[this.i].bike_stands,
-					stationStandAvailable: this.data[this.i].available_bike_stands,
-					stationBikeAvailable: this.data[this.i].available_bikes,
-					stationBanking: this.data[this.i].banking
-				});
-				console.log(this.marker)
-	
-				this.marker.addListener("click", ()=> {
-					console.log(this.marker);
+			if (nbMarker == -1){
+				nbMarker = this.data.length;
+			}
+			for (this.i=0; this.i < nbMarker ; this.i++){
 				
-					document.getElementById("station-name").innerHTML = this.marker.stationName;
-	
-					if (this.marker.stationStatus == 'OPEN') {
-						document.getElementById("station-status").innerHTML = 'ouverte';
-						document.getElementById("station-status").className = 'station-open';
-					} else if (this.marker.stationStatus == 'CLOSE') {
-						document.getElementById("station-status").innerHTML = 'fermée';
-						document.getElementById("station-status").className = 'station-close';
-					}
-	
-					this.banking = this.marker.stationBanking;
-					if (this.banking) {
-						document.getElementById("station-banking").style.color = "black";
-					} else if (!this.banking) {
-						document.getElementById("station-banking").style.color = "grey";
-					}
-	
-					document.getElementById("station-address").innerHTML = this.marker.stationAddress;
-					document.getElementById("station-available-bike").innerHTML = this.marker.stationBikeAvailable;
-					document.getElementById("station-total-stand").innerHTML = this.marker.stationTotalStand;
-					document.getElementById('info-container').style.width = "300px";
-					document.getElementById('station-details').style.display = "block";
-				});
-	
-				document.getElementById("close").addEventListener('click', (e)=> {
-				document.getElementById('info-container').style.width = "0px";
-				document.getElementById('station-details').style.display = "none";
-				});
-	
-				document.getElementById("station-btn").addEventListener('click', (e)=> {
-				document.getElementById("canvas-section").style.display = "block";
-				});
-				
+			this.addinfoMarker(	this.data[this.i].position.lat,this.data[this.i].position.lng,this.map,this.data[this.i].number,this.data[this.i].name,this.data[this.i].address,this.data[this.i].status,
+			this.data[this.i].bike_stands,this.data[this.i].available_bike_stands,this.data[this.i].available_bikes,this.data[this.i].banking);
 			}
 		});
-	
-		this.ajax.send();
+
+	this.ajax.send();
+
 	}
+	
 }
